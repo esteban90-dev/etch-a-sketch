934488de0970e800892cc5ef2d42157713bcceec
Stephen Allard <stephencallard@gmail.com>
2019-11-18 18:35:21 -0600

    now users can input grid size

--- scripts/script.js
+++ scripts/script.js
@@ -2,49 +2,87 @@
 let gridContainer;
 let newRow;
 let newSquare;
-let numSquare = 20;
+let numSquare;
 let squareSize;
 let square;
+let reset;
+let userIn;
 let x;
 let y;
 let z;
 
-//calculate square size
-squareSize = 600/numSquare;
 
+function createGrid(p1){
+    numSquare = p1;
+    //calculate square size
+    squareSize = 600/numSquare;     //width/height of grid container is 600px
 
-//populate the grid with squares
-gridContainer = document.querySelector(".main");
+    //clear grid - delete all children divs of main
+    gridContainer = document.querySelector(".main");
+    gridContainer.innerHTML = "";
 
-for(x = 0; x < numSquare; x++){
+    //populate the grid with squares
+    for(x = 0; x < numSquare; x++){
 
-    newRow = document.createElement("div");
-    newRow.className = 'row-container';
-    gridContainer.appendChild(newRow);
+        newRow = document.createElement("div");
+        newRow.className = 'row-container';
+        gridContainer.appendChild(newRow);
 
-    for(y = 0; y < numSquare; y++){
+        for(y = 0; y < numSquare; y++){
+            
+            newSquare = document.createElement("div");
+            newSquare.className = 'square';
         
-        newSquare = document.createElement("div");
-        newSquare.className = 'square';
-    
-        newSquare.style.width = squareSize + "px";
-        newSquare.style.height = squareSize + "px";
+            newSquare.style.width = squareSize + "px";
+            newSquare.style.height = squareSize + "px";
         
-    
-        newRow.appendChild(newSquare);
+            newRow.appendChild(newSquare);
+            
+        }
+
+    }
+
+    //after grid is created, add mouseover events for each square
+    square = document.querySelectorAll(".square");
+    for(z = 0; z < square.length; z++){
+        square[z].addEventListener("mouseover",changeColor);
     }
 
 }
 
+//create default grid when user loads site - 25 squares per row
+if(!userIn){
+    createGrid(50);
+}
 
-//add event to change square color when mouse hovers over
+//event function to change square color on mouseover
 function changeColor(e){
     e.target.style.backgroundColor = 'rgb(88, 88, 88)';
-    //e.target.style.color = 'black';
 }
 
-square = document.querySelectorAll(".square");
-for(z = 0; z < square.length; z++){
-    square[z].addEventListener("mouseover",changeColor);
+//add events for reset button
+function enHighlight(e){
+    e.target.className = 'reset-button-highlight';
 }
+
+function disHighlight(e){
+    e.target.className = 'reset-button';
+}
+
+function resetGrid(){
+    userIn = 0;
+    while(!userIn){
+        userIn = prompt('Enter how many squares per row');
+    }
+    createGrid(userIn);
+}
+
+reset = document.querySelector(".reset-button");
+reset.addEventListener("mouseover",enHighlight);
+reset.addEventListener("mouseout",disHighlight);
+reset.addEventListener("click",resetGrid);
+
+
+
+
 
